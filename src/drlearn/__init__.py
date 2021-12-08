7ed7454b3f4791c96f359093c8940caf771d9b85
Javicadserres <javier.cardenas.serres@gmail.com>
2021-12-08 22:10:00 +0100

    ENH: add versioneer

--- src/drlearn/__init__.py
+++ src/drlearn/__init__.py
@@ -3,6 +3,8 @@ from . import utils
 
 from .drlearn import DRLearn
 from .utils import Memory
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+# from ._version import get_versions
+# __version__ = get_versions()['version']
+# del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
